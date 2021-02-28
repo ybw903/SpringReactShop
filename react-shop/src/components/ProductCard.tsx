1d5e96eaa75d41d47b3bd5abc8caf4b5584d2721
병우 유 <conner903@naver.com>
2021-02-28 13:10:36 +0900

    REFACTOR: Cart Component add dispatch

--- react-shop/src/components/ProductCard.tsx
+++ react-shop/src/components/ProductCard.tsx
@@ -3,23 +3,36 @@ import {
     Card,
     Button
 } from 'react-bootstrap';
+import { connect, ConnectedProps } from 'react-redux';
+import { addToCart } from '../actions/cart';
 import { Product } from '../types/products';
 
-type Props = {
+const mapDispatch = {
+    onAddToCart : (product : Product) => addToCart(product),
+}
+
+const connector = connect(
+    null,
+    mapDispatch
+);
+
+type PropsFromRedux = ConnectedProps<typeof connector>
+
+type Props = PropsFromRedux&{
     product: Product
 }
 
-const ProductCard = ({product}: Props) => {
+const ProductCard = ({onAddToCart, product}: Props) => {
     return(
         <Card>
             <Card.Body>
                 <Card.Title>{product.productName}</Card.Title>
                 <Card.Text>{product.productDescription}</Card.Text>
                 <span>{product.productPrice}</span>
-                <Button className="cart-button" variant="outline-primary">카트에 담기</Button>
+                <Button className="cart-button" variant="outline-primary" onClick={()=>onAddToCart(product)}>카트에 담기</Button>
             </Card.Body>
         </Card>
     )
 }
 
-export default ProductCard;
+export default connector(ProductCard);
